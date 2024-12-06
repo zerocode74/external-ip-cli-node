76172378edb5914baa620b5d6a7b25ed5a4f5c1b
Renan Souza <renan.souza@powerpush.com.br>
2024-12-06 08:49:54 -0300

    fix: changed the provider

--- ipx.js
+++ ipx.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 
 const getExternalIp = async () => {
-    const response = await axios.get('https://api.bigdatacloud.net/data/client-ip');
+    const response = await axios.get('https://api-bdc.net/data/client-info');
     const ip = response.data.ipString;
     return ip;
 }
